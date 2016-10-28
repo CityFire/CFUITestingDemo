51e19f9be290cb3ad62c52cb791a8951f09905b4
CityFire <13787697167@163.com>
2016-10-28 16:18:23 +0800

    Initial Commit

---
+++ CFUITestingDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CFUITestingDemo
+//
+//  Created by wjc on 16/10/28.
+//  Copyright © 2016年 wjc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
